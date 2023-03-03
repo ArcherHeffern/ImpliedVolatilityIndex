58894cd947ffe75b2f269c94b810ca050b685e50
Noam <noammolloy@gmail.com>
2023-03-03 14:10:43 -0500

    added graph component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,3 +54,95 @@
         </article>
     </div>
 </div>
+
+<style>
+    body {
+        background-color: whitesmoke;
+    }
+
+    p.tablecat{
+        font-family: 'Times New Roman', Times, serif;
+        border: solid cornflowerblue;
+        margin-right: 120px;
+    }
+
+    .flex-container-table{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .tablecat-container{
+        margin-top: 30px;
+    }
+
+    #table{
+        padding-left: 300px;
+        padding-bottom: 300px;
+        padding-top: 300px;
+        padding-right: 300px;
+        outline-style: solid;
+    }
+
+    h1{
+        text-align: center;
+        margin-top: 100px;
+    }
+
+    .flex-container-trending{
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .news-item{
+        text-align: center;
+        border: solid cornflowerblue;
+        margin-left: 200px;
+        margin-right: 200px;
+        padding-top: 50px;
+    }
+
+    footer{
+        background-color: cornflowerblue;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style>
+
+
+<html lang="en-US">
+<body>
+
+<div class="flex-container-table">
+<div class="tablecat-container">
+
+<p class="tablecat"> 100 Most Popular </p>
+<p class="tablecat"> Tech </p>
+<p class="tablecat"> Energy </p>
+<p class="tablecat"> Healthcare </p>
+<p class="tablecat"> etc </p>
+
+</div>
+
+<p id="table"> This is a data table </p>
+</div>
+
+<div class="flex-container-trending">
+<h1> Trending Now </h1>
+<p class="news-item"> Trending news item 1 </p>
+<p class="news-item"> Trending news item 2 </p>
+<p class="news-item"> Trending news item 4 </p>
+<p class="news-item"> Trending news item etc... </p>
+</div>
+
+<footer>
+  <p> contact@email.com </p>
+</footer>
+
+
+
+</body>
+</html>
