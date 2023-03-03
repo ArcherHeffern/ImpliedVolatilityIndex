6fb96e7c93ded2b5411ee24ef4e18e3819fb0631
Archer <heffernarcher@gmail.com>
2023-03-03 13:01:45 -0500

    fixed header and main page bugs with porting

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,3 +6,60 @@ has links to stuff:
 <a href="/signup">Signup</a>
 <a href="/portfolio">Portfolio</a>
 <a href="/team">Team</a>
+<div class="header">
+    <div class="header-left">
+        <img src="logo.png" height="50px" width="auto" alt="quant logo">
+    </div>
+
+    <div class="header-right">
+        <div class="container-search">
+            <div class="topnav">
+                <input type="text" placeholder="Search..">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6 col-sm-4">User Profile</div>
+            <div class="col-6 col-sm-4">Portfolio</div>
+            <div class="col-6 col-sm-4">Settings</div>
+        </div>
+    </div>
+ </div>
+
+ <style>
+        .header {
+        background: #88b3eb;
+        display: flex;
+        padding: 20px;
+        border-bottom: 1px solid #e9e9e9;
+    }
+
+    .header-left {
+        flex-grow: 1;
+        display: flex;
+        gap: 10px;
+        text-align: left;
+    }
+
+    .topnav {
+        overflow: hidden;
+        background-color: #e9e9e9;
+    }
+
+    .header-right {
+        color: black;
+        text-align: right;
+    }
+
+    .container-search {
+        padding-top: 4px;
+        display: flex;
+        justify-content: center;
+        padding-bottom: 4px;
+    }
+
+    a {
+        color: gray;
+        text-decoration: none;
+    }
+ </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,16 @@
+<script>
+    
+</script>
+
 <style>
     * {
         margin: 0;
         padding: 0;
     }
 
-    body {
+    .body {
         height: 200vh;
         background: rgb(255, 255, 255);
-    }
-
-    header {
-        background: #88b3eb;
-        display: flex;
-        padding: 20px;
-        border-bottom: 1px solid #e9e9e9;
-    }
-
-    .header-left {
-        flex-grow: 1;
-        display: flex;
-        gap: 10px;
-        text-align: left;
-    }
-
-    .topnav {
-        overflow: hidden;
-        background-color: #e9e9e9;
-    }
-
-    .title {
-        color: black;
-        text-align: center;
-        display: flex;
-        justify-content: center;
-    }
-
-    .header-right {
-        color: black;
-        text-align: right;
-    }
-
-    .container-search {
-        padding-top: 4px;
-        display: flex;
-        justify-content: center;
-        padding-bottom: 4px;
     }
 
     .container-list {
@@ -66,39 +32,10 @@
         padding-left: 10px;
     }
 
-    a {
-        color: gray;
-        text-decoration: none;
-    }
-
 </style>
 
-<script>
-    
-</script>
-
-<body>
-    <header>
-       <div class="header-left">
-           <img src="logo.png" height="50px" width="auto" alt="quant logo">
-       </div>
-
-       <div class="header-right">
-           <div class="container-search">
-               <div class="topnav">
-                   <input type="text" placeholder="Search..">
-               </div>
-           </div>
-
-           <div class="row">
-               <div class="col-6 col-sm-4">User Profile</div>
-               <div class="col-6 col-sm-4">Portfolio</div>
-               <div class="col-6 col-sm-4">Settings</div>
-           </div>
-       </div>
-    </header>
-
-    <main>
+<div class='body'>
+    <div>
        <article>
            <div class="container-list">
                <div class="container-graph-content">
@@ -113,10 +50,7 @@
                <div class="container-graph">
                    <img src="dow.png" height="200px" width="auto" alt="graph">
                </div>
-               
            </div>
         </article>
-    </main>
-
-    <footer></footer>
-</body>
+    </div>
+</div>
