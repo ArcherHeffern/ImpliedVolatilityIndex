de13829ef42aa53ab505982a9401ac76f53e4f6e
Rsirp0c <122833217+Rsirp0c@users.noreply.github.com>
2023-03-21 16:22:21 -0400

    fixed header design

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,29 @@
+<div class = "main">
+    <div id = "mainHeader">
+        <div id = "containerlogo">
+            <img id = "quant-logo" src="logo.png" alt="Quant logo">
+        </div>
+        <div id="container_middle">
+        </div>
+        <div id = "container_navbar">
+            <a href="/">stock</a>
+            <a href="/portfolio">Portfolio</a>
+            <a href="/team">About</a>
+            <a href="/login">Log in</a>    
+        </div>
+        <div id="countainer_signup">
+            <a href="/signup"><img id = "signup_button" src="signup.png" alt="signup button"></a>    
+        </div>
+    </div>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100&display=swap" rel="stylesheet">
+</div>
+
+
 <style>
+/* The following are <div> setting */
 .main {
-    font-family: 'Chivo', monospace;
-    background-color: rgb(237, 237, 237);
+    font-family: 'Outfit', sans-serif;
+    background-color: white;
     /* margin: 0.2rem; */
 }
 /* .main {
@@ -13,30 +35,64 @@
     height: 70px;
     width: 100%;
     background-color: white;
-    border-radius: 7px;
+    /* border-radius: 7px;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    border: 0px black solid;
+    border: 0px black solid; */
 }
-#container1 {
+#containerlogo {
     /* border: 2px solid red; */
-    width: 70%;
+    width: 10%;
     display: flex;
     justify-content: flex-start;
     height: 70px;
+    margin: 0 10px 0 0;
 }
-#container2 {
+#quant-logo {
+    /* border: 2px solid black; */
+    height: 60px;
+    width: 265px;
+    margin: 12px -5px 0 30px;
+}
+
+
+#container_middle {
+    width: 40%;    
+    /* display: flex;
+        border-style: none;
+        justify-content:space-around;
+        text-align: left;
+        margin-left: 15px; */
+        
+}
+#container_navbar {
     /* border: 2px solid blue; */
-    width: 30%;
+    width: 35%;
     display: flex;
-    justify-content:space-around;
-    gap: 20px;
+    justify-content:right;
+    gap: 40px;
     height: 70px;
     margin: 0 10px 0 0;
 }
+#countainer_signup {
+    width: 15%;
+    display: flex;
+    justify-content:space-around;
+    height: 70px;
+    margin: 0 10px 0 0;
+}
+#signup_button {
+    height: 80px;
+    width: 140px;
+    margin: -17px 0 0 0;
+}
+
+/* The following are font setting */
+
 a {
     margin: 25px 0px 0 0;
     height: 20px;
     text-decoration: none;
+    font-size:28px;
 }
 a:link {
     color: black;
@@ -54,55 +110,6 @@ a:visited {
 a:visited:hover {  
     color: rgb(255, 85, 85);
 }
-#quant-logo {
-    /* border: 2px solid black; */
-    height: 50px;
-    margin: 12px -5px 0 30px;
-}
-.one {
-    font-size: 1.5rem;
-    /* border: black 2px solid; */
-    margin: 8px 0 0 0;
-}
-.two {
-    font-size: 1.5rem;
-    /* border: black 2px solid; */
-    margin: -10px 0 0 0;
-}
-section {
-    display: flex;
-    gap: 20px;
-}
-#logo-text {
-    /* border: 2px red solid; */
-    width: 60%;
-    display: flex;
-    flex-wrap: wrap;
-}
+
 </style>
 <!-- This section will need some logic to check if the user is logged in or not - this will have to be implemented in conjeunction with user authentication -->
-<div class = "main">
-    <div id = "mainHeader">
-        <div id = "container1">
-            <section>
-                <img id = "quant-logo" src="quant-logo.png" alt="Quant logo">
-                <div id = "logo-text">
-                    <p class = "one"> Brandeis University </p>
-                    <p class = "two"> Quant Club</p>
-                </div>
-            </section>
-        </div>
-        <div id = "container2">
-            <a href="/">Main</a>
-            <a href="/login">Log in</a>
-            <a href="/signup">Signup</a>
-            <a href="/portfolio">Portfolio</a>
-            <a href="/team">Team</a>
-        </div>
-    </div>
-    <link href="https://fonts.googleapis.com/css2?family=Chivo:wght@600&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
-</div>
-
-<script>
-
-</script>
