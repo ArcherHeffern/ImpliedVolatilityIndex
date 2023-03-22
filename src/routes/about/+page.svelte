67d8b3eed2d7a0f5a29e72ed00f7e608e9271fd1
erichurchey <erichurchey@brandeis.edu>
2023-03-21 23:31:45 -0400

    Create +page.svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,179 @@
+<style>
+  h1{
+      text-align: center;
+      margin-top: 30px;
+  }
+  
+  .name{
+    font-size: 30px;
+  }
+
+  .directorClass{
+    font-size: 25px;
+  }
+
+  .titlePositions{
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+      text-align: left;
+      margin-left: 45px;
+      width: 76%;
+      padding: 0 90px;
+      margin-top: 50px;
+      margin-bottom: 40px;
+      font-size: 30px;
+  }
+
+  .p1{
+      font-size: 30px;
+      margin-bottom: 5px;
+      margin-top: 30px;
+  }
+
+  .photoOfMembers{
+      text-align: center;
+  }
+
+*, *:before, *:after {
+box-sizing: inherit;
+}
+
+.column {
+float: left;
+width: 35%;
+margin-bottom: 16px;
+margin-left: 116px;
+/* padding: 0 90px; */
+}
+
+.container {
+padding: 0 16px;
+}
+
+.container::after, .row::after {
+content: "";
+clear: both;
+display: table;
+}
+
+@media screen and (max-width: 650px) {
+.column {
+  width: 100%;
+  display: block;
+}
+}
+
+hr {
+border: none;
+height: 2px;
+color: #333;
+background-color: #333;
+}
+
+</style>
+
+
+
+<main>
+<div class="aboutTitle">
+  <h1>About the Team</h1>
+</div>
+
+<div class="titlePositions">
+  <h2>Members</h2>
+</div>
+
+<div class="photoOfMembers">
+  <img src="logo.png" height="150px" width="auto" alt="quant logo" >
+</div>
+
+<div class="titlePositions">
+  <h2>Directors</h2>
+  <hr>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create 
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create 
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="titlePositions">
+  <h2>Web Engineer Team</h2>
+  <hr>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create 
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create 
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="titlePositions">
+  <h2>Math Team</h2>
+  <hr>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create I will put
+        some more text here to test out the length of this section lol xd testing testing testing
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="container">
+      <h2 class="name">Daniel Archerson <img src="logo.png" height="50px" width="auto" alt="quant logo" > </h2>
+      <h3 class="directorClass">Director of Morale</h3>
+      <p class="p1">Hello! I am Daniel and this is just some random text to test out what I am trying to create 
+        bodybod ybodybodyb odybodybodybod ybodybodybod ybodybodybody
+        body 
+      </p>
+    </div>
+  </div>
+</div>
+
+</main>
