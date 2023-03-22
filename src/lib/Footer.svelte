fd6720634c4d934c17e3fd0c1709071cab0e0eae
Archer <heffernarcher@gmail.com>
2023-03-22 01:43:29 -0400

    cleaned up footer code

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,94 +1,53 @@
-<div id="footer-main">
-    <div id="footer-top">
-        <div id="footer-left">
-            <p1 class="footer-h1">© Brandeis Quant Club. All Rights Reserved</p1>
-        </div>
-        <div id="footer-middle">
-        </div>
-        <div id="footer-right">
-            <a href="/team">About us</a>
-            <a href="https://brandeisquantclub.org/">Quant Club Website</a>
-            <a href="https://www.linkedin.com/company/brandeisquants/"><img id = "linkedin-logo" src="Linkedin.png" alt="quant logo"></a>    
-        </div>
+<div id="footer-top">
+    <div id="footer-left">
+        <p1 class="footer-h1">© Brandeis Quant Club. All Rights Reserved</p1>
     </div>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100&display=swap" rel="stylesheet">
+    <div id="footer-right">
+        <a href="/team">About us</a>
+        <a href="https://brandeisquantclub.org/">Quant Club Website</a>
+        <a href="https://www.linkedin.com/company/brandeisquants/"><img id = "linkedin-logo" src="Linkedin.png" alt="quant logo"></a>    
+    </div>
 </div>
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100&display=swap" rel="stylesheet">
 
 <style>
-   
-    #footer-main {
-    font-family: 'Outfit', sans-serif;
-    background-color: white;
-    /* margin: 0.2rem; */
-    }
     #footer-top {
+        background-color: white;
+        font-family: 'Outfit', sans-serif;
         display: flex;
+        justify-content: space-between;
+        align-items: center;
         height: 70px;
         width: 100%;
-        /* background-color: white;
-        border-radius: 7px;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        border: 0px black solid; */
     }
     #footer-left {
         display: flex;
+        flex-grow: 1;
         border-style: none;
-        justify-content:space-around;
         text-align: left;
-        margin-left: 15px;
-        width: 30%;
-    }
-
-    #footer-middle {
-        /* display: flex;
-        border-style: none;
-        justify-content:space-around;
-        text-align: left;
-        margin-left: 15px; */
-        width: 40%;
+        margin-left: 30px;
     }
     #footer-right {
         display: flex;
+        align-items: center;
         justify-content:space-around;
-        gap: 20px;
-        height: 70px;
-        margin: 0 10px 0 0;
-        width: 30%;
+        gap: 27px;
+        margin-right: 30px;
     }
     #linkedin-logo {
-    /* border: 2px solid black; */
     height: 50px;
     width: 50px;
-    margin: -13px 0 10px 0;
     }   
     .footer-h1 {
         font-size: 20px;
-        
-        margin: 25px 0px 0 0;
-        height: 20px;
-        /* margin-bottom: 1px; */
     }
     a {
-    margin: 27px 0px 0 0;
-    height: 20px;
     text-decoration: none;
-    font-size:20px;
-    
-    }
-    a:link {
-        color: black;
+    font-size: 20px;
+    color: black;
     }
     a:hover {  
-        color: rgb(255, 85, 85);
-    }
-    a:active {
-        text-decoration: none; 
-    }
-    a:visited {
-        color: black;
-        text-decoration: none; 
-    }
-    a:visited:hover {  
-        color: rgb(255, 85, 85);
+        color: #38caef;
+        transition: all .1s;
     }
 </style>
